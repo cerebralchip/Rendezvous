94975810b0450e8cb3190e67ef14512cc30d5f8b
Rapha Taylor <raphataylor1@gmail.com>
2024-03-06 17:10:37 +0000

    first url mapping done

--- rendezvous/rendezvous/urls.py
+++ rendezvous/rendezvous/urls.py
@@ -16,7 +16,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+
+from rendezvous_app import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
+    path("map/", views.map, name="map"),
+    path("discover/", views.discover, name="discover"),
+    path("resources/", views.resources, name="resources"),
+    path("profile/", views.profile, name="profile"),
+    path("settings/", views.settings, name="settings"),
+    path("about/", views.about, name="about"),
+    path("login/", views.login, name="login"),
+    path("register/", views.register, name="register"),
+    path("country/", views.country, name="country"),
+    path("search/", views.search, name="search"),
+    path("search_results/", views.search_results, name="search_results"),
+    path("comment/", views.comment, name="comment"),
     path("admin/", admin.site.urls),
 ]
