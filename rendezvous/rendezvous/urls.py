3cb9bda0ced3ad27ad331c63e2ea83c68816f3a5
Alexander Robertson <alex@davie.com>
2024-03-22 16:53:00 +0000

    hjfgfgfgh

--- rendezvous/rendezvous/urls.py
+++ rendezvous/rendezvous/urls.py
@@ -51,7 +51,7 @@ urlpatterns = [
     path("comment/", views.comment, name="comment"),
     path("admin/", admin.site.urls),
     path("post/<int:post_id>/", views.post_detail, name="post_detail"),
-    path('create_post/', views.create_post, name='create_post'),
+    path('create-post/', views.create_post, name='create_post'),
 
     path('api/recent-posts/', views.get_recent_posts, name='recent-posts'),
     path('api/popular-posts/', views.get_popular_posts, name='popular-posts'),
