8a58fac04ea69ed4148a73ec7a1f89eff1b1e795
Rapha Taylor <raphataylor1@gmail.com>
2024-03-22 16:41:43 +0000

    fixed profile view and template

--- rendezvous/rendezvous_app/views.py
+++ rendezvous/rendezvous_app/views.py
@@ -72,7 +72,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            return redirect('home')
+            return redirect('index')
     else:
         form = AuthenticationForm()
     return render(request, 'rendezvous/login.html', {'form': form, 'active_page': 'login'})
