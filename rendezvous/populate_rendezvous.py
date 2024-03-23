299b7a8b7718514f4e8fcd30ee97d804ef9260f6
Fernando Perez <fdoiv7@gmail.com>
2024-03-23 07:28:33 -0600

    Added example posts

--- rendezvous/populate_rendezvous.py
+++ rendezvous/populate_rendezvous.py
@@ -52,12 +52,156 @@ def populate():
     # Create posts
     posts = [
         # Generate 5 example posts
+<<<<<<< HEAD
         {'PostID': 1, 'UserID_id': 1, 'Picture': 'Screenshot_2024-03-02_at_17.27.57_7W7smpP.png', 'Title': 'Hello', 'Text': 'Exploring the Grand Canyon', 'CountryID_id': 1, 'is_featured': True},
         {'PostID': 2, 'UserID_id': 2, 'Picture': 'Screenshot_2024-03-02_at_17.27.57.png', 'Title': 'World', 'Text': 'Eating fish and chips in London', 'Upvotes':'4', 'CountryID_id': 2, 'is_featured': True},
         {'PostID': 3, 'UserID_id': 1, 'Picture': 'Screenshot_2024-03-17_at_20.22.35.png', 'Title': 'testing', 'Text': 'Visiting the Eiffel Tower', 'CountryID_id': 3},
         {'PostID': 4, 'UserID_id': 2, 'Picture': 'Screenshot_2024-03-18_at_15.17.31.png', 'Title': '1234', 'Text': 'Hiking in the Scottish Highlands', 'Upvotes':'12', 'CountryID_id': 2, },
         {'PostID': 5, 'UserID_id': 1, 'Picture': '', 'Title': 'foobar','Text': 'Trying sushi in Tokyo', 'CountryID_id': 1},
         {'PostID': 6, 'UserID_id': 2, 'Picture': 'PXL_20230606_211615824.MP.jpg', 'Title': 'baz', 'Text': 'Exploring the Great Wall of China', 'CountryID_id': 4},
+=======
+        {
+            'PostID': 1,
+            'UserID_id': 1,
+            'Picture': 'Screenshot_2024-03-02_at_17.27.57_7W7smpP.png', 
+            'Title': 'An Unforgettable Journey Through England: From Historical Landmarks to Natural Splendors', 
+            'Text': """Embark on a journey through England, where each step tells a story, and every landscape inspires a sonnet.
+                    Our adventure begins in the bustling streets of London, where the grandeur of history meets the vibrancy of modern life.
+                    Here, the Tower of London stands as a testament to centuries past, while the London Eye offers a panorama of a city constantly evolving. 
+                    As we traverse the cobbled lanes of the capital, we encounter flavors that span the globe, yet the essence of traditional English cuisine 
+                    remains a must for any culinary explorer. The echoes of Shakespeare's verses lead us to Stratford-upon-Avon, 
+                    inviting us to ponder the age-old question: 'To be, or not to be?' The tranquility of the Lake District offers a moment of reflection, 
+                    and the historic stones of Stonehenge remain a mystery, reminding us of England's deep history. 
+                    In Cornwall, the rugged coastlines and charming fishing villages paint a picture of England's maritime heritage.
+                    Throughout this journey, England reveals itself not just through its sights but through its spirit, 
+                    inviting travelers to immerse themselves in a land where the past and present dance in harmony.""",
+            'CountryID_id': 1,
+            'is_featured': True,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Culture'},
+                {'TagID': 3, 'TagName': 'Guide and Tips'}
+            ]
+        },
+        {
+            'PostID': 2,
+            'UserID_id': 2, 
+            'Picture': 'Screenshot_2024-03-02_at_17.27.57.png', 
+            'Title': 'Discovering Scotland: A Land of History, Mystery, and Natural Beauty', 
+            'Text': """Journey into the heart of Scotland, where the echoes of ancient battles mingle with the peaceful solitude of vast, unspoiled landscapes. 
+                    Our exploration begins in the historic alleyways of Edinburgh, under the watchful eye of Edinburgh Castle, perched atop its volcanic throne. 
+                    As we delve into the mysteries of Rosslyn Chapel, whispers of the Knights Templar and the Holy Grail add a layer of intrigue to our adventure.""", 
+            'Upvotes':'4', 
+            'CountryID_id': 2, 
+            'is_featured': True,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Culture'},
+                {'TagID': 3, 'TagName': 'History'}
+            ]
+        },
+        {
+            'PostID': 3, 
+            'UserID_id': 1, 
+            'Picture': 'Screenshot_2024-03-17_at_20.22.35.png', 
+            'Title': 'testing', 
+            'Text': """Dive deep into the heart of Mexico, a country where every corner tells a story of history, resilience, and vibrant culture. 
+                    Our journey begins in the bustling streets of Mexico City, home to ancient ruins and modern murals that speak volumes of the nation's past and present.
+                    As we explore further, the culinary landscapes unfold with the rich flavors of traditional dishes like tacos al pastor, mole, and chiles en nogada, 
+                    each a masterpiece of local ingredients and culinary heritage. Venturing beyond the cities, the natural wonders of Mexico await—from the crystalline 
+                    cenotes of Yucatán to the majestic Copper Canyon in Chihuahua, offering breathtaking views and adventures for every type of traveler. 
+                    Along the way, the warmth of the Mexican people and their festive traditions, from the vibrant Día de Muertos celebrations to the tranquil siestas,
+                    invite visitors to partake in the joy of life that pulses through the country. This post is an ode to Mexico's enduring spirit, a call to explore 
+                    its vast landscapes, indulge in its culinary delights, and immerse oneself in its rich cultural tapestry.""", 
+            'CountryID_id': 3,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Culture'},
+                {'TagID': 3, 'TagName': 'Eats'},
+            ]
+        },
+        {
+            'PostID': 4,
+            'UserID_id': 2,
+            'Picture': 'Screenshot_2024-03-18_at_15.17.31.png',
+            'Title': 'Greece: A Voyage to the Cradle of Civilization and Beyond',
+            'Text': """ Embark on an epic journey to Greece, where ancient ruins whisper tales of democracy, philosophy, and heroism under the azure skies. 
+                    Our adventure begins amidst the hallowed halls of the Acropolis in Athens, where the Parthenon stands as a beacon of ancient glory and 
+                    architectural marvel. As we delve deeper into the heart of Greece, we encounter the rugged beauty of the Peloponnese, the mystical oracle 
+                    at Delphi, and the serene monasteries of Meteora, perched high upon towering rocks. The Greek islands beckon with their own tales, 
+                    from the labyrinthine alleys of Crete's Knossos to the sapphire waters of Mykonos. Greek cuisine, with its emphasis on fresh ingredients 
+                    and timeless recipes, offers a feast for the senses—savor the flavors of olives, feta, and succulent lamb under a starlit sky. 
+                    This post is an invitation to experience Greece's rich history, breathtaking landscapes, and vibrant culture—a journey where every moment 
+                    is a bridge between the past and the present.""",
+            'Upvotes':'12',
+            'CountryID_id': 2,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Culture'},
+                {'TagID': 3, 'TagName': 'Eats'},
+            ]
+        },
+        {
+            'PostID': 5,
+            'UserID_id': 1,
+            'Picture': '', 
+            'Title': 'A Journey Through France: From Lavish Artistry to Rustic Vineyards',
+            'Text': """Embark on an enchanting journey through France, a country synonymous with elegance, gastronomy, and unparalleled art. 
+                    Our voyage begins in the luminous streets of Paris, the City of Lights, where the majestic Eiffel Tower and the sprawling 
+                    Louvre Museum beckon. As we meander through the cobblestone lanes, the aroma of freshly baked croissants and the sight of chic 
+                    patisseries tempt our senses, inviting us to savor the culinary delights that define French cuisine. Beyond the urban allure, 
+                    the rolling hills and verdant vineyards of Bordeaux and Provence offer a taste of France's esteemed winemaking tradition, 
+                    with each glass telling a story of the land and its people. Our exploration leads us to the rugged coasts of Brittany and Normandy, 
+                    where the sea whispers tales of ancient mariners and the winds carry the legacy of impressionist painters who once captured the fleeting 
+                    moments of natural beauty. This post is a tribute to France's rich tapestry of culture, history, and artistry, a call to wander its picturesque
+                    villages, indulge in its gourmet offerings, and immerse oneself in the joie de vivre that radiates from every corner of this splendid country.""",
+            'CountryID_id': 1,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Culture'},
+                {'TagID': 3, 'TagName': 'Eats'},
+            ]
+        },
+        {
+            'PostID': 6,
+            'UserID_id': 2,
+            'Picture': '',
+            'Title': 'Singapore: A Fusion of Cultures, Gastronomy, and Futuristic Visions',
+            'Text': """Embark on a journey to Singapore, a small island city-state that stands as a testament to extraordinary innovation and cultural amalgamation. 
+                    Our exploration begins amidst the futuristic landscapes of Marina Bay, where architectural marvels meet green technology, epitomizing Singapore's 
+                    vision for the future. As we weave through the bustling streets, the harmonious blend of Malay, Chinese, Indian, and Western cultures becomes evident, 
+                    not just in the diverse languages spoken but in the vibrant festivals and daily life. Culinary adventurers will delight in Singapore's hawker centers, 
+                    where Michelin-starred meals sit alongside traditional laksa, Hainanese chicken rice, and chili crab, offering a taste of the nation's rich gastronomic heritage. 
+                    Beyond the urban spectacle, the lush greenery of the Botanic Gardens and the enchanting nocturnal wildlife at the Night Safari reveal Singapore's commitment 
+                    to nature and conservation. This post invites readers to discover Singapore's multifaceted charm, from its towering skyscrapers and lush parks to its melting 
+                    pot of cuisines and cultures, all coexisting in harmony on this futuristic island.""",
+            'CountryID_id': 1,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Culture'},
+                {'TagID': 3, 'TagName': 'Eats'},
+            ]
+        },
+        {
+            'PostID': 7,
+            'UserID_id': 1,
+            'Picture': '',
+            'Title': 'Australia: A Vast Land of Breathtaking Contrasts, From Red Deserts to Lush Rainforests',
+            'Text': """Embark on an epic journey across Australia, a continent that defies imagination with its diverse landscapes and rich cultural heritage. 
+                    Our adventure spans from the iconic red sands of the Outback, where Uluru stands as a sacred monument, to the vibrant coral ecosystems of the Great Barrier Reef, 
+                    teeming with life beneath the waves. In cities like Sydney and Melbourne, a cosmopolitan vibe thrives, offering world-class dining, art, and entertainment, 
+                    while the laid-back surf culture of the Gold Coast beckons with its pristine beaches. Venturing into the heart of Tasmania, the ancient rainforests offer a 
+                    tranquil retreat into nature's untouched beauty. Along the way, encounter Australia's unique wildlife, from the curious kangaroo to the elusive platypus, 
+                    each a wonder in its own right. This post is a tribute to Australia's spirit of adventure, inviting readers to explore its contrasts, 
+                    from the desert's serene expanse to the bustling urban scenes and serene coastal towns, each with stories to tell and mysteries to unveil.""",
+            'CountryID_id': 1,
+            'tags' : [
+                {'TagID': 1, 'TagName': 'Explore'},
+                {'TagID': 2, 'TagName': 'Nature'},
+                {'TagID': 3, 'TagName': 'Adventure'},
+            ]
+        }
+>>>>>>> e7ec58a (Added example posts)
         # Add more posts as needed
     ]
 
@@ -74,7 +218,6 @@ def populate():
 
     for comment_data in comments:
         Comment.objects.get_or_create(**comment_data)
-
     
 
 if __name__ == '__main__':
